66322eea3c7d6fb2abfaa07ad307fe31aa7d374e
Abdul Basit <basitawan.abdul@gmail.com>
2021-12-07 12:41:03 +0500

    Update telr.blade.php

--- resources/views/testing/telr.blade.php
+++ resources/views/testing/telr.blade.php
@@ -7,7 +7,7 @@
     <input name="ivp_authkey" type="text" value="11f01e2eMgskkV$7C9z54XPg"><br>
     <input name="ivp_amount" type="text" value="19.95"><br>
     <input name="ivp_currency" type="text" value="AED"><br>
-    <input name="ivp_test" type="text" value="1"><br>
+    <input name="ivp_test" type="text" value="0"><br>
     <input name="ivp_timestamp" type="text" value="1293002624"><br>
     <input name="ivp_cart" type="text" value="ABC123"><br>
     <input name="ivp_desc" type="text" value="Items"><br>
